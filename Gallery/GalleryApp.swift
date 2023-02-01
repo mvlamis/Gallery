dd84c5c12af4a5cb816d2d101918e35cb98ad486
Michael Vlamis <michael.vlamis@icloud.com>
2023-01-31 21:38:43 -0500

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Michael Vlamis on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
