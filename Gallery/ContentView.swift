ee624c3153b9f209b64c26350809efaaeb49737d
Michael Vlamis <michael.vlamis@icloud.com>
2023-02-01 12:19:04 -0500

    start

--- Gallery/ContentView.swift
+++ Gallery/ContentView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Image("placeholder")
+                    .resizable()
+                    .frame(width: 175, height: 175)
+                Image("placeholder")
+                    .resizable()
+                    .frame(width: 175, height: 175)
+                
+                
+            }
+            
+            
         }
-        .padding()
+
+        
     }
+    
+    
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
 }
